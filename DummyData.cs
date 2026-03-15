288ceffc3a0e697c0775767996437e01544627cc
Archie Watson Irvine <157597766+ArchieWatsonIrvine@users.noreply.github.com>
2026-03-15 15:33:56 +0000

    Add files via upload

---
+++ DummyData.cs
@@ -0,0 +1,13 @@
+﻿using NottsBAAPI.DTO.Auth;
+using NottsBAAPI.Models.Auth;
+
+namespace NottsBAAPI;
+
+public class DummyData
+{
+    public static List<User> Users = new List<User>()
+    {
+         new User() { Id = 1, Username = "admin", Password = "admin", Role = "Admin" },
+         new User() { Id = 2, Username = "user", Password = "user", Role = "User" }
+    };
+}
